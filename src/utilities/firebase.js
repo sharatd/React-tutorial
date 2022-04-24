a7af7159977447b9226efa366b0f8ec601f39d03
sharatd <sharat.lakers24@gmail.com>
2022-04-23 22:17:48 -0500

    add login

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,16 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, onValue, ref, set } from 'firebase/database';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
+
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, set } from "firebase/database";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onIdTokenChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoIQ6Tw3aSdUJyWUHGzswfHQq70cYuBUc",
-  authDomain: "scheduler-16335.firebaseapp.com",
-  databaseURL: "https://scheduler-16335-default-rtdb.firebaseio.com",
-  projectId: "scheduler-16335",
-  storageBucket: "scheduler-16335.appspot.com",
-  messagingSenderId: "247970157406",
-  appId: "1:247970157406:web:599891401eead8687a75cf",
-  measurementId: "G-4BYW56VV2D"
+  apiKey: "AIzaSyB9SLPggynvPZHM7lToY3pmBgtMGaZ63z4",
+  authDomain: "scheduler-56832.firebaseapp.com",
+  databaseURL: "https://scheduler-56832-default-rtdb.firebaseio.com",
+  projectId: "scheduler-56832",
+  storageBucket: "scheduler-56832.appspot.com",
+  messagingSenderId: "35620246912",
+  appId: "1:35620246912:web:50e924a744aa9d407b5d65",
 };
 
 const firebase = initializeApp(firebaseConfig);
@@ -23,24 +30,46 @@ export const useData = (path, transform) => {
 
   useEffect(() => {
     const dbRef = ref(database, path);
-    const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-    if (devMode) { console.log(`loading ${path}`); }
-    return onValue(dbRef, (snapshot) => {
-      const val = snapshot.val();
-      if (devMode) { console.log(val); }
-      setData(transform ? transform(val) : val);
-      setLoading(false);
-      setError(null);
-    }, (error) => {
-      setData(null);
-      setLoading(false);
-      setError(error);
-    });
+    const devMode =
+      !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+    if (devMode) console.log(`loading ${path}`);
+
+    return onValue(
+      dbRef,
+      (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) console.log(val);
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      },
+      (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      }
+    );
   }, [path, transform]);
 
   return [data, loading, error];
 };
 
-export const setData = (path, value) => (
-  set(ref(database, path), value)
-);
+export const setData = (path, value) => set(ref(database, path), value);
+
+export const signInWithGoogle = () => {
+  signInWithPopup(getAuth(firebase), new GoogleAuthProvider());
+};
+
+const firebaseSignOut = () => signOut(getAuth(firebase));
+
+export { firebaseSignOut as signOut };
+
+export const useUserState = () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onIdTokenChanged(getAuth(firebase), setUser);
+  }, []);
+
+  return [user];
+};
